fba1c45cf11576998528e8fce1f28776b841b3a8
nishant.kumar <nk.muzaffarpur@gmail.com>
2017-02-07 00:36:57 +0530

    removed redundant code

--- web.js
+++ web.js
@@ -5,10 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
 
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
   response.render('/index');
 });
